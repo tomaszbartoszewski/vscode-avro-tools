555b99a8e36e8a8bb5826545db1760e339f149f7
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2020-08-22 13:55:40 +0100

    Change tokenizer and buildTree test to check if outside and inside node numbe of items is as expected

--- server/test/syntaxtree_parsing.test.ts
+++ server/test/syntaxtree_parsing.test.ts
@@ -11,7 +11,7 @@ function countTokensInNode(node: any): number {
 		return 1;
 	}
 	else if (node instanceof ArrayNode) {
-		var count = 0;
+		let count = 0;
 		if (node.leftBracket !== null) {
 			count++;
 		}
@@ -24,14 +24,14 @@ function countTokensInNode(node: any): number {
 		return count;
 	}
 	else if (node instanceof ArrayItem) {
-		var count = countTokensInNode(node.value);
+		let count = countTokensInNode(node.value);
 		if (node.comma !== null) {
 			count++;
 		}
 		return count;
 	}
 	else if (node instanceof Node) {
-		var count = 0;
+		let count = 0;
 		if (node.leftBracket !== null) {
 			count++;
 		}
@@ -44,7 +44,7 @@ function countTokensInNode(node: any): number {
 		return count;
 	}
 	else if (node instanceof KeyValuePair) {
-		var count = countTokensInNode(node.value);
+		let count = countTokensInNode(node.value);
 		if (node.key !== null) {
 			count++;
 		}
@@ -56,31 +56,28 @@ function countTokensInNode(node: any): number {
 		}
 		return count;
 	}
-	else if (node instanceof Tree) {
-		return countTokensInNode(node.node) + node.outside.length
-	}
-
 }
 
 describe('Run tokenize and buildTree together', () => {
 	it('Check number of tokens in a tree', () => {
-		const textNumberOfTokens: [string, number][] = [
-			['{\n\r"type"  :\t  "string"  \n}', 5],
-			['{"a": {"b"}}', 7],
-			['{"a": {"b": [1,2,3]}}', 15],
-			['{"a": {"b": [1,2,3', 12],
-			['{"a": {"b": [1,2,3], "c": "asd"}}', 19],
-			['{"a": 4, "c": "asd"}', 9],
-			['{"a": null, "c": false, "d": -123.43, "e": true}', 17],
-			// ['{"a": 4, "c": "asd"}}', 10],
+		const textNumberOfTokens: [string, number, number][] = [
+			['{\n\r"type"  :\t  "string"  \n}', 5, 0],
+			['{"a": {"b"}}', 7, 0],
+			['{"a": {"b": [1,2,3]}}', 15, 0],
+			['{"a": {"b": [1,2,3', 12, 0],
+			['{"a": {"b": [1,2,3], "c": "asd"}}', 19, 0],
+			['{"a": 4, "c": "asd"}', 9, 0],
+			['{"a": null, "c": false, "d": -123.43, "e": true}', 17, 0],
+			['{"a": 4, "c": "asd"}}', 9, 1],
 		];
 
 		textNumberOfTokens.forEach(function(tn) {
-			var [text, numberOfTokens] = tn;
+			const [text, numberOfTokens, outside] = tn;
 			const tokens = tokenize(text);
-			assert.equal(tokens.length, numberOfTokens);
+			assert.equal(tokens.length, numberOfTokens + outside);
 			const tree = buildTree(tokens);
-			assert.equal(countTokensInNode(tree), numberOfTokens);
+			assert.equal(countTokensInNode(tree.node), numberOfTokens);
+			assert.equal(tree.outside.length, outside);
 		});
 	});
 });
