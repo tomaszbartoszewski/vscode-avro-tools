d0ac516d869e5f65673ebc8a52e71bab9c26b4e9
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2020-08-25 21:21:50 +0100

    Validate namespace based on regex

--- server/src/validation/namesAndSymbolsValidator.ts
+++ server/src/validation/namesAndSymbolsValidator.ts
@@ -4,7 +4,8 @@ import { Validator, ValidationMessage, ValidationMessageAggregator, ValidationSe
 import { CorrectSchemaWalker } from './correctSchemaWalker';
 
 export class NamesAndSymbolsValidator implements Validator {
-	private nameRegex = new RegExp('^[A-Za-z_][A-Za-z0-9_]*$');
+	private nameRegex = new RegExp('^\"[A-Za-z_][A-Za-z0-9_]*\"$');
+	private namespaceRegex = new RegExp('^\"[A-Za-z_][A-Za-z0-9_]*(\.[A-Za-z_][A-Za-z0-9_]*)*\"$');
 
 	validate(tree: Tree): ValidationMessage[] {
 		const messageAggregator = new ValidationMessageAggregator();
@@ -16,13 +17,22 @@ export class NamesAndSymbolsValidator implements Validator {
 	}
 
 	private validateNode(node: ObjectNode, messageAggregator: ValidationMessageAggregator, isField: boolean = false) {
+		if (isField) {
+			const nameAttribute = node.attributes.find(kv => kv.key !== null && kv.key.value === '"name"');
+			this.validateName(nameAttribute, messageAggregator);
+			const namespaceAttribute = node.attributes.find(kv => kv.key !== null && kv.key.value === '"namespace"');
+			this.validateNamespace(namespaceAttribute, messageAggregator);
+		}
+		
 		const type = node.attributes.find(kv => kv.key !== null && kv.key.value === '"type"');
 		if (type instanceof KeyValuePair && type.value instanceof StringToken) {
 			const token: StringToken = type.value;
 			console.log(token);
-			if (token.value === '"record"' && !isField) {
+			if (token.value === '"record"' && !isField) { // if it's a field we already ran a validation
 				const nameAttribute = node.attributes.find(kv => kv.key !== null && kv.key.value === '"name"');
 				this.validateName(nameAttribute, messageAggregator);
+				const namespaceAttribute = node.attributes.find(kv => kv.key !== null && kv.key.value === '"namespace"');
+				this.validateNamespace(namespaceAttribute, messageAggregator);
 			}
 		}
 	}
@@ -32,11 +42,27 @@ export class NamesAndSymbolsValidator implements Validator {
 			const name = attribute.value.value;
 			console.log(name);
 			if (!this.nameRegex.test(name)) {
+				console.log('Name failed')
 				messageAggregator.addMessage(new ValidationMessage(
 					ValidationSeverity.Error,
 					attribute.value.getStartPosition(),
 					attribute.value.getEndPosition(),
-					'Name ' + name + ' is not matching a regular expression ' + this.nameRegex.source));
+					'Name ' + name + ' is not matching a regular expression [A-Za-z_][A-Za-z0-9_]*'));
+			}
+		}
+	}
+
+	private validateNamespace(attribute: KeyValuePair | undefined, messageAggregator: ValidationMessageAggregator) {
+		if (attribute instanceof KeyValuePair && attribute.value instanceof StringToken) {
+			const namespace = attribute.value.value;
+			console.log(namespace);
+			if (!this.namespaceRegex.test(namespace)) {
+				console.log('Namespace failed')
+				messageAggregator.addMessage(new ValidationMessage(
+					ValidationSeverity.Error,
+					attribute.value.getStartPosition(),
+					attribute.value.getEndPosition(),
+					'Namespace ' + namespace + ' is not matching a regular expression [A-Za-z_][A-Za-z0-9_]*(\.[A-Za-z_][A-Za-z0-9_]*)*'));
 			}
 		}
 	}
--- server/test/namesAndSymbolsValidator.test.ts
+++ server/test/namesAndSymbolsValidator.test.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert';
-import { keyValuePair, nodeWithoutBrackets, validObjectNodeWithType, objectNode, arrayNodeWithoutBrackets, arrayItem } from './syntaxTreeUtils';
-import { StringToken, IntegerToken, ColonToken, CommaToken } from '../src/tokens';
+import { keyValuePair, nodeWithoutBrackets, validObjectNodeWithType, arrayNodeWithoutBrackets } from './syntaxTreeUtils';
+import { StringToken, IntegerToken, CommaToken } from '../src/tokens';
 import { NamesAndSymbolsValidator } from '../src/validation/namesAndSymbolsValidator'
 import { Tree } from '../src/syntaxTree';
 import { ValidationMessage, ValidationSeverity } from '../src/validation/validators';
@@ -20,17 +20,17 @@ describe('NamesAndSymbolsValidator', () => {
 			ValidationSeverity.Error,
 			30,
 			34,
-			'Name "11" is not matching a regular expression ^[A-Za-z_][A-Za-z0-9_]*$'));
+			'Name "11" is not matching a regular expression [A-Za-z_][A-Za-z0-9_]*'));
 	});
 
 	[
 		['', false],
-		['0Name', false],
-		['£_Name', false],
-		['N', true],
-		['a', true],
-		['Name_test_12', true],
-		['something76QWERTY', true]
+		['"0Name"', false],
+		['"£_Name"', false],
+		['"N"', true],
+		['"a"', true],
+		['"Name_test_12"', true],
+		['"something76QWERTY"', true]
 	].forEach(([name, isValid]: [string, boolean]) => {
 		it('validate name: ' + name + ' expected is valid: ' + isValid, () => {
 			const node = nodeWithoutBrackets(
@@ -72,9 +72,78 @@ describe('NamesAndSymbolsValidator', () => {
 		const fields = arrayNodeWithoutBrackets(childNode);
 
 		const node = nodeWithoutBrackets(
-			keyValuePair(new StringToken('"name"', 1), new ColonToken(':', 7), new StringToken('"Test"', 9), new CommaToken(',', 15)),
-			keyValuePair(new StringToken('"type"', 20), new ColonToken(':', 27), new StringToken('"record"', 29), null),
-			keyValuePair(new StringToken('"fields"', 35), new ColonToken(':', 43), fields, null),
+			keyValuePair(new StringToken('"name"', 1), null, new StringToken('"Test"', 9), new CommaToken(',', 15)),
+			keyValuePair(new StringToken('"type"', 20), null, new StringToken('"record"', 29), null),
+			keyValuePair(new StringToken('"fields"', 35), null, fields, null),
+		);
+		const highlights = validator.validate(new Tree(node, []));
+		assert.equal(highlights.length, 1);
+	});
+
+	it('validate incorrect namespace', () => {
+		const node = nodeWithoutBrackets(
+			keyValuePair(new StringToken('"type"', 0), null, new StringToken('"record"', 10), null),
+			keyValuePair(new StringToken('"name"', 20), null, new StringToken('"Test"', 30), null),
+			keyValuePair(new StringToken('"namespace"', 38), null, new StringToken('"1234"', 50), null),
+		);
+
+		const highlights = validator.validate(new Tree(node, []));
+		assert.equal(highlights.length, 1);
+		assert.deepEqual(highlights[0], new ValidationMessage(
+			ValidationSeverity.Error,
+			50,
+			56,
+			'Namespace "1234" is not matching a regular expression [A-Za-z_][A-Za-z0-9_]*(\.[A-Za-z_][A-Za-z0-9_]*)*'));
+	});
+
+	[
+		['', false],
+		['"0Name"', false],
+		['"£_Name"', false],
+		['"Name."', false],
+		['".Name"', false],
+		['"N"', true],
+		['"a"', true],
+		['"Name_test_12"', true],
+		['"something76QWERTY"', true],
+		['"something.name.test"', true],
+	].forEach(([namespace, isValid]: [string, boolean]) => {
+		it('validate namespace: ' + namespace + ' expected is valid: ' + isValid, () => {
+			const node = nodeWithoutBrackets(
+				keyValuePair(new StringToken('"type"', 0), null, new StringToken('"record"', 10), null),
+				keyValuePair(new StringToken('"name"', 20), null, new StringToken('"Test"', 30), null),
+				keyValuePair(new StringToken('"namespace"', 40), null, new StringToken(namespace, 50), null)
+			);
+
+			assert.equal(validator.validate(new Tree(node, [])).length === 0, isValid);
+		});
+	});
+
+	it ('validate namespace of nested object', () => {
+		const childNode = nodeWithoutBrackets(
+			keyValuePair(new StringToken('"type"', 10), null, new StringToken('"record"', 18), null),
+			keyValuePair(new StringToken('"name"', 20), null, new StringToken('"Test"', 30), null),
+			keyValuePair(new StringToken('"namespace"', 40), null, new StringToken('"%%^&"', 55), null)
+		);
+
+		const node = validObjectNodeWithType(childNode);
+		const highlights = validator.validate(new Tree(node, []));
+		assert.equal(highlights.length, 1);
+	});
+
+	it ('validate name of field', () => {
+		const childNode = nodeWithoutBrackets(
+			keyValuePair(new StringToken('"type"', 10), null, new StringToken('"string"', 18), null),
+			keyValuePair(new StringToken('"name"', 20), null, new StringToken('"Test"', 30), null),
+			keyValuePair(new StringToken('"namespace"', 40), null, new StringToken('"%%^&"', 55), null)
+		);
+
+		const fields = arrayNodeWithoutBrackets(childNode);
+
+		const node = nodeWithoutBrackets(
+			keyValuePair(new StringToken('"name"', 1), null, new StringToken('"Test"', 9), new CommaToken(',', 15)),
+			keyValuePair(new StringToken('"type"', 20), null, new StringToken('"record"', 29), null),
+			keyValuePair(new StringToken('"fields"', 35), null, fields, null),
 		);
 		const highlights = validator.validate(new Tree(node, []));
 		assert.equal(highlights.length, 1);
