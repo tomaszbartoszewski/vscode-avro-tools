99bffbca034d77b9786bbf3aab9657ce4278e134
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2020-08-29 20:03:29 +0100

    Add validation for default values of type boolean and int

--- server/src/validation/defaultValidator.ts
+++ server/src/validation/defaultValidator.ts
@@ -1,7 +1,8 @@
 import { Validator, ValidationMessage, ValidationMessageAggregator, ValidationSeverity } from './validators';
 import { Tree, ObjectNode, KeyValuePair } from '../syntaxTree';
 import { CorrectSchemaWalker } from './correctSchemaWalker';
-import { StringToken, NullToken } from '../tokens';
+import { StringToken, NullToken, BoolToken, IntegerToken } from '../tokens';
+import { HighlightRange } from '../highlightsRange';
 
 export class DefaultValidator implements Validator {
 	validate(tree: Tree): ValidationMessage[] {
@@ -14,6 +15,14 @@ export class DefaultValidator implements Validator {
 	}
 
 	private validateNode(node: ObjectNode, messageAggregator: ValidationMessageAggregator, isField: boolean) {
+		const addErrorMessage = function (range: HighlightRange, msg: string) {
+			messageAggregator.addMessage(new ValidationMessage(
+				ValidationSeverity.Error,
+				range.getStartPosition(),
+				range.getEndPosition(),
+				msg));
+		}
+		
 		if (isField) {
 			const typeAttribute = node.attributes.find(kv => kv.key !== null && kv.key.value === '"type"');
 			const defaultAttribute = node.attributes.find(kv => kv.key !== null && kv.key.value === '"default"');
@@ -21,11 +30,22 @@ export class DefaultValidator implements Validator {
 				if (typeAttribute instanceof KeyValuePair && typeAttribute.value instanceof StringToken) {
 					const typeToken: StringToken = typeAttribute.value;
 					if (typeToken.value === '"null"' && !(defaultAttribute.value instanceof NullToken)) {
-						messageAggregator.addMessage(new ValidationMessage(
-							ValidationSeverity.Error,
-							defaultAttribute.getStartPosition(),
-							defaultAttribute.getEndPosition(),
-							'Default value for type "null" has to be a null'));
+						addErrorMessage(defaultAttribute, 'Default value for type "null" has to be a null');
+					}
+					else if (typeToken.value === '"boolean"' && !(defaultAttribute.value instanceof BoolToken)) {
+						addErrorMessage(defaultAttribute, 'Default value for type "boolean" has to be true or false');
+					}
+					else if (typeToken.value === '"int"') {
+						if (defaultAttribute.value instanceof IntegerToken) {
+							const token = defaultAttribute.value;
+							const intValue = Number(token.value);
+							if (intValue > 2147483647 || intValue < -2147483648) {
+								addErrorMessage(defaultAttribute, 'Default value for type "int" has to be a 32-bit signed integer');
+							}
+						}
+						else {
+							addErrorMessage(defaultAttribute, 'Default value for type "int" has to be a 32-bit signed integer');
+						}
 					}
 				}
 			}
--- server/test/defaultValidator.test.ts
+++ server/test/defaultValidator.test.ts
@@ -2,7 +2,7 @@ import * as assert from 'assert';
 import { KeyValuePair, ArrayNode, ObjectNode, Tree, ArrayItem } from '../src/syntaxTree';
 import { Validator, ValidationMessage, ValidationSeverity } from '../src/validation/validators'
 import { DefaultValidator } from '../src/validation/defaultValidator'
-import { StringToken, Token, LeftBracketToken, RightBracketToken, IntegerToken, NullToken } from '../src/tokens';
+import { StringToken, Token, LeftBracketToken, RightBracketToken, IntegerToken, NullToken, BoolToken, PrecisionNumberToken } from '../src/tokens';
 import { nodeWithoutBrackets, keyValuePair, arrayNodeWithoutBrackets, validRecordWithField } from './syntaxTreeUtils';
 
 describe('DefaultValidator', () => {
@@ -29,5 +29,61 @@ describe('DefaultValidator', () => {
 		);
 		const highlights = validator.validate(new Tree(node, []));
 		assert.equal(highlights.length, 0);
+	});
+
+	it('Boolean type can only have default boolean', () => {
+		const node = validRecordWithField(
+			keyValuePair(new StringToken('"type"', 20), null, new StringToken('"boolean"', 30), null),
+			keyValuePair(new StringToken('"default"', 40), null, new IntegerToken('11', 50), null)
+		);
+		const highlights = validator.validate(new Tree(node, []));
+		assert.equal(highlights.length, 1);
+		assert.deepEqual(highlights[0], new ValidationMessage(
+			ValidationSeverity.Error,
+			40,
+			52,
+			'Default value for type "boolean" has to be true or false'));
+	});
+
+	it('Boolean type correct default', () => {
+		const node = validRecordWithField(
+			keyValuePair(new StringToken('"type"', 20), null, new StringToken('"boolean"', 30), null),
+			keyValuePair(new StringToken('"default"', 40), null, new BoolToken('false', 50), null)
+		);
+		const highlights = validator.validate(new Tree(node, []));
+		assert.equal(highlights.length, 0);
+	});
+
+	it('Type int can only have integer number', () => {
+		const node = validRecordWithField(
+			keyValuePair(new StringToken('"type"', 20), null, new StringToken('"int"', 30), null),
+			keyValuePair(new StringToken('"default"', 40), null, new PrecisionNumberToken('1.0', 50), null)
+		);
+		const highlights = validator.validate(new Tree(node, []));
+		assert.equal(highlights.length, 1);
+		assert.deepEqual(highlights[0], new ValidationMessage(
+			ValidationSeverity.Error,
+			40,
+			53,
+			'Default value for type "int" has to be a 32-bit signed integer'));
+	});
+
+	[
+		['2147483647', 0],
+		['-2147483648', 0],
+		['12', 0],
+		['-545656', 0],
+		['2147483648', 1],
+		['-2147483649', 1],
+		['2147483647354645', 1],
+	].forEach(([value, numberOfErrors]: [string, number]) => {
+		it('Type int can only have 32-bit signed integer number value: ' + value + ' number of errors ' + numberOfErrors, () => {
+			const node = validRecordWithField(
+				keyValuePair(new StringToken('"type"', 20), null, new StringToken('"int"', 30), null),
+				keyValuePair(new StringToken('"default"', 40), null, new IntegerToken(value, 50), null)
+			);
+			const highlights = validator.validate(new Tree(node, []));
+			assert.equal(highlights.length, numberOfErrors);
+		});
 	});
 });
