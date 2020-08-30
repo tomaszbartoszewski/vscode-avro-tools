c941d96ecb412f51bb6d18ca57b3e4e5d582ccb5
tomaszbartoszewski <tomasz.bartoszewski@gmail.com>
2020-08-30 14:11:31 +0100

    Extract validation and generating error messages based on type to two functions for future validation

--- server/src/validation/defaultValidator.ts
+++ server/src/validation/defaultValidator.ts
@@ -1,7 +1,7 @@
 import { Validator, ValidationMessage, ValidationMessageAggregator, ValidationSeverity } from './validators';
 import { Tree, ObjectNode, KeyValuePair, ArrayNode } from '../syntaxTree';
 import { CorrectSchemaWalker } from './correctSchemaWalker';
-import { StringToken, NullToken, BoolToken, IntegerToken, PrecisionNumberToken } from '../tokens';
+import { StringToken, NullToken, BoolToken, IntegerToken, PrecisionNumberToken, Token } from '../tokens';
 import { HighlightRange } from '../highlightsRange';
 
 export class DefaultValidator implements Validator {
@@ -31,62 +31,23 @@ export class DefaultValidator implements Validator {
 			if (defaultAttribute instanceof KeyValuePair) {
 				if (typeAttribute instanceof KeyValuePair && typeAttribute.value instanceof StringToken) {
 					const typeToken: StringToken = typeAttribute.value;
-					if (typeToken.value === '"null"' && !(defaultAttribute.value instanceof NullToken)) {
-						addErrorMessage(defaultAttribute, 'Default value for type "null" has to be a null');
-					}
-					else if (typeToken.value === '"boolean"' && !(defaultAttribute.value instanceof BoolToken)) {
-						addErrorMessage(defaultAttribute, 'Default value for type "boolean" has to be true or false');
-					}
-					else if (typeToken.value === '"int"') {
-						if (defaultAttribute.value instanceof IntegerToken) {
-							const token = defaultAttribute.value;
-							const intValue = Number(token.value);
-							if (intValue > 2147483647 || intValue < -2147483648) {
-								addErrorMessage(defaultAttribute, 'Default value for type "int" has to be a 32-bit signed integer');
-							}
-						}
-						else {
-							addErrorMessage(defaultAttribute, 'Default value for type "int" has to be a 32-bit signed integer');
-						}
-					}
-					else if (typeToken.value === '"long"' && !(defaultAttribute.value instanceof IntegerToken)) { // TODO: Add extra checks to see if it is a 64-bit number, so far what I read JS supports 53-bit
-						addErrorMessage(defaultAttribute, 'Default value for type "long" has to be a 64-bit signed integer');
-					}
-					else if (typeToken.value === '"float"' && !(defaultAttribute.value instanceof PrecisionNumberToken) && !(defaultAttribute.value instanceof IntegerToken)) { // TODO: Add extra checks to see if it is a 32-bit number
-						addErrorMessage(defaultAttribute, 'Default value for type "float" has to be a 32-bit single precision floating-point number');
-					}
-					else if (typeToken.value === '"double"' && !(defaultAttribute.value instanceof PrecisionNumberToken) && !(defaultAttribute.value instanceof IntegerToken)) { // TODO: Add extra checks to see if it is a 64-bit number
-						addErrorMessage(defaultAttribute, 'Default value for type "double" has to be a 64-bit double precision floating-point number');
-					}
-					else if (typeToken.value === '"string"' && !(defaultAttribute.value instanceof StringToken)) {
-						addErrorMessage(defaultAttribute, 'Default value for type "string" has to be a string');
-					}
-					else if (typeToken.value === '"bytes"' && !this.isCorrectUnicodeDefault(defaultAttribute)) {
-						addErrorMessage(defaultAttribute, 'Default value for type "bytes" has to be a string containing Unicode codes 0-255 in a format \\u00FF\\u0048');
-					}
-					else if (typeToken.value === '"fixed"' && !this.isCorrectUnicodeDefault(defaultAttribute)) {
-						addErrorMessage(defaultAttribute, 'Default value for type "fixed" has to be a string containing Unicode codes 0-255 in a format \\u00FF\\u0048');
-					}
-					else if (typeToken.value === '"enum"') {
-						const symbolsAttribute = node.attributes.find(kv => kv.key !== null && kv.key.value === '"symbols"');
-						if (symbolsAttribute instanceof KeyValuePair && !this.isCorrectEnumDefault(defaultAttribute, symbolsAttribute)){
-							addErrorMessage(defaultAttribute, 'Default value for type "enum" has to be a string from symbols array');
-						}
+					if (!this.isValidDefaultForType(typeToken, defaultAttribute, node)) {
+						addErrorMessage(defaultAttribute, this.getErrorMessageForType(typeToken));
 					}
 				}
 			}
 		}
 	}
 
-	private isCorrectUnicodeDefault(defaultAttribute: KeyValuePair): boolean {
-		return defaultAttribute.value instanceof StringToken && this.bytesDefaultRegex.test(defaultAttribute.value.value);
+	private isCorrectUnicodeDefault(defaultValue: Token | ObjectNode | ArrayNode | null ): boolean {
+		return defaultValue instanceof StringToken && this.bytesDefaultRegex.test(defaultValue.value);
 	}
 
-	private isCorrectEnumDefault(defaultAttribute: KeyValuePair, symbols: KeyValuePair): boolean {
-		if (!(defaultAttribute.value instanceof StringToken)) {
+	private isCorrectEnumDefault(defaultValue: Token | ObjectNode | ArrayNode | null, symbols: KeyValuePair): boolean {
+		if (!(defaultValue instanceof StringToken)) {
 			return false;
 		}
-		const defaultText = defaultAttribute.value.value;
+		const defaultText = defaultValue.value;
 		let isOnSymbols = false;
 		if (symbols.value instanceof ArrayNode) {
 			symbols.value.items.forEach(symbol => {
@@ -99,5 +60,78 @@ export class DefaultValidator implements Validator {
 			});
 		}
 		return isOnSymbols;
+	}
+
+	private isValidDefaultForType(typeToken: StringToken, defaultAttribute: KeyValuePair, node: ObjectNode): boolean {
+		if (typeToken.value === '"null"' && !(defaultAttribute.value instanceof NullToken)) {
+			return false;
+		}
+		else if (typeToken.value === '"boolean"' && !(defaultAttribute.value instanceof BoolToken)) {
+			return false;
+		}
+		else if (typeToken.value === '"int"') {
+			if (defaultAttribute.value instanceof IntegerToken) {
+				const token = defaultAttribute.value;
+				const intValue = Number(token.value);
+				if (intValue > 2147483647 || intValue < -2147483648) {
+					return false;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+		else if (typeToken.value === '"long"' && !(defaultAttribute.value instanceof IntegerToken)) { // TODO: Add extra checks to see if it is a 64-bit number, so far what I read JS supports 53-bit
+			return false;
+		}
+		else if (typeToken.value === '"float"' && !(defaultAttribute.value instanceof PrecisionNumberToken) && !(defaultAttribute.value instanceof IntegerToken)) { // TODO: Add extra checks to see if it is a 32-bit number
+			return false;
+		}
+		else if (typeToken.value === '"double"' && !(defaultAttribute.value instanceof PrecisionNumberToken) && !(defaultAttribute.value instanceof IntegerToken)) { // TODO: Add extra checks to see if it is a 64-bit number
+			return false;
+		}
+		else if (typeToken.value === '"string"' && !(defaultAttribute.value instanceof StringToken)) {
+			return false;
+		}
+		else if (typeToken.value === '"bytes"' && !this.isCorrectUnicodeDefault(defaultAttribute.value)) {
+			return false;
+		}
+		else if (typeToken.value === '"fixed"' && !this.isCorrectUnicodeDefault(defaultAttribute.value)) {
+			return false;
+		}
+		else if (typeToken.value === '"enum"') {
+			const symbolsAttribute = node.attributes.find(kv => kv.key !== null && kv.key.value === '"symbols"');
+			if (symbolsAttribute instanceof KeyValuePair && !this.isCorrectEnumDefault(defaultAttribute.value, symbolsAttribute)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private getErrorMessageForType(typeToken: StringToken): string {
+		switch (typeToken.value) {
+			case '"null"':
+				return 'Default value for type "null" has to be a null';
+			case '"boolean"':
+				return 'Default value for type "boolean" has to be true or false';
+			case '"int"':
+				return 'Default value for type "int" has to be a 32-bit signed integer';
+			case '"long"':
+				return 'Default value for type "long" has to be a 64-bit signed integer';
+			case '"float"':
+				return 'Default value for type "float" has to be a 32-bit single precision floating-point number';
+			case '"double"':
+				return 'Default value for type "double" has to be a 64-bit double precision floating-point number';
+			case '"string"':
+				return 'Default value for type "string" has to be a string';
+			case '"bytes"':
+				return 'Default value for type "bytes" has to be a string containing Unicode codes 0-255 in a format \\u00FF\\u0048';
+			case '"fixed"':
+				return 'Default value for type "fixed" has to be a string containing Unicode codes 0-255 in a format \\u00FF\\u0048';
+			case '"enum"':
+				return 'Default value for type "enum" has to be a string from symbols array';
+			default:
+				return 'Default value is not matching type';
+		}
 	}
 }
